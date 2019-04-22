7d9a335330dd2561cf14993a14ddcd7394121359
ABHISHEK KUMAR <abhishekyadav8060@gmail.com>
2019-04-22 22:12:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+var ques = document.getElementById('ques');
+var res = document.getElementById('res');
+
+
+function startConverting () {
+                
+                var finalTranscripts = '';
+				if('webkitSpeechRecognition' in window){
+					var speechRecognizer = new webkitSpeechRecognition();
+					speechRecognizer.continuous = true;
+					speechRecognizer.interimResults = true;
+					speechRecognizer.lang = 'en-IN';
+					speechRecognizer.start();
+
+					
+
+					speechRecognizer.onresult = function(event){
+						var interimTranscripts = '';
+						for(var i = event.resultIndex; i < event.results.length; i++){
+							var transcript = event.results[i][0].transcript;
+							transcript.replace("\n", "<br>");
+							if(event.results[i].isFinal){
+								finalTranscripts += transcript;
+							}else{
+								interimTranscripts += transcript;
+							}
+						}
+						ques.innerHTML = finalTranscripts + '<span style="color:#999">' + interimTranscripts + '</span>';
+                       
+                        
+                        res.innerHTML=showAnswer(finalTranscripts);
+                        
+                        
+                        
+					};
+					speechRecognizer.onerror = function (event) {
+					};
+				}else{
+					ques.innerHTML = 'Your browser is not supported. If google chrome, please upgrade!';
+				}
+            
+            
+    
+			}
+
+function showAnswer(ques){
+    
+    var q=["what is your name?","how are you?","where do you live?","are you married?","aur laude?"];
+    var a=["Abhishek","Fine","Prayagraj","No","Bol Bhosdike"];
+    
+    //console.log();
+    
+    for(i=0;i<q.length;i++){
+        
+        if(ques==q[i]){
+            return a[i];
+        }
+        
+    }
+    
+    return "looking for the answer...";
+}
